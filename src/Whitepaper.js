61ca5a655bd7d87bea3f4d365a64a05a80ec8d23
Nnorom-coder <nnorom.prince44@gmail.com>
2022-12-25 14:53:43 +0100

    Whitepaper

--- src/Whitepaper.js
+++ src/Whitepaper.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { Document, Page, pdfjs } from 'react-pdf'
+import DownloadIcon from '@mui/icons-material/Download';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import './whitepaper.css'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
@@ -24,13 +27,19 @@ const Whitepaper = () => {
   }
   return (
     <div>
-    <button className='DownloadButton' onClick={() => downloadPDF()}> Download</button>
-           <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-        {
+    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', position: 'fixed', width: '100vw', zIndex: 1,}}>
+    {
           pageNum <= 1
-            ? <button className='WhitePaperPrevDisabled' disabled> Prev</button>
-            : <button className='WhitePaperButtonPrev' onClick={() => goToPrevPage()}> Prev</button>
-        }
+            ? ''
+            : <button className='WhitePaperButtonPrev' onClick={() => goToPrevPage()}><ArrowBackIosIcon /></button>
+    }
+    <DownloadIcon  className='DownloadButton' onClick={() => downloadPDF()} />
+    {
+            pageNum >= numOfPages
+            ? ''
+            : <button className='WhitePaperButtonNext' onClick={() => goToNextPage()}><ArrowForwardIosIcon /></button>
+    }
+    </div>
         <div style={{ textAlign: 'center', display: 'flex', justifyContent: 'center'}}>
         <Document
         file='/whitepaper.pdf'
@@ -39,15 +48,7 @@ const Whitepaper = () => {
         >
             <Page pageNumber={pageNum} size='A4'/>
         </Document>
-        </div>
-        {
-            pageNum >= numOfPages
-            ?
-            <button className='WhitePaperNextDisabled' disabled> Next</button>
-            :  
-            <button className='WhitePaperButtonNext' onClick={() => goToNextPage()}> Next</button>
-        }
-        </div>    
+        </div>   
     </div>
   )
 }
